83e67490db5e4dff0a38fe6c136360f3e3b2afa4
xShadex <bartek.kwiczala@gmail.com>
2018-09-18 20:58:43 +0200

    styled footer

--- shopreact/src/components/Shop/Footer.js
+++ shopreact/src/components/Shop/Footer.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import "../Style/output.css";
+
 
 class Footer extends Component {
   render() {
     return (
       <div className="_Footer">
+      <div>
+        <p>Copyright by my-shop - 2018</p>
+      </div>
       </div>
     );
   }
