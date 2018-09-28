65b8641932f2e8a91330f5110d2d5115a15688eb
xShadex <bartek.kwiczala@gmail.com>
2018-09-28 15:29:36 +0200

    fixed bugs

--- shopreact/src/App.js
+++ shopreact/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/Shop/Header";
 import Footer from "./components/Shop/Footer";
-import MainPage from "./components/Shop/Main_Page";
+import Products from "./components/Shop/Products";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <MainPage />
+        <Products />
         <Footer/>
       </div>
     );
